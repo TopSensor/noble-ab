25a528fc612791c1831e5b7ecc54ca7fb036687e
eugenioenr <65408391+eugenioenr@users.noreply.github.com>
2020-08-29 07:11:02 -0300

    Sudo btmgmt

--- enableBle.sh
+++ enableBle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ln -s $SNAP/lib/arm-linux-gnueabihf/libreadline.so.7.0 $SNAP/lib/arm-linux-gnueabihf/libreadline.so.6
-$SNAP/usr/bin/btmgmt power on 
+#ln -s $SNAP/lib/arm-linux-gnueabihf/libreadline.so.7.0 $SNAP/lib/arm-linux-gnueabihf/libreadline.so.6
+sudo $SNAP/usr/bin/btmgmt power on 
