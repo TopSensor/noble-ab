c6b00255175398c57a42a7419b7eabd0a31406bd
eugenioesg <43115660+eugenioesg@users.noreply.github.com>
2020-08-30 14:59:50 -0300

    Delete enableBle.sh
    everything in command chain

--- enableBle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#ln -s $SNAP/lib/arm-linux-gnueabihf/libreadline.so.7.0 $SNAP/lib/arm-linux-gnueabihf/libreadline.so.6
-ngrok http -region=sa -hostname=tsbt.sa.ngrok.io 3000
-ngrok tcp --remote-addr 3.tcp.ngrok.io:23398 22
-sudo $SNAP/usr/bin/btmgmt power on 
